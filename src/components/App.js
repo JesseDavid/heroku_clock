db3b001a3be972dd0168f3e221d4678d95832c93
Steven Shaginyan <steven.shaginyan@heroku.com>
2018-05-11 12:40:38 -0700

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
         super();
 
         this.state = {
-            title: 'Heroku Clock by Steven. Added sentence.'
+            title: 'Heroku Clock by Steven. Added sentence!'
         };
     }
 
