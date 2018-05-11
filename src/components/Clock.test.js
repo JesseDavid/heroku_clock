01c8e3855adb8060d3ceec1ac8a00699a0d624f3
Steven Shaginyan <steven.shaginyan@heroku.com>
2018-05-11 11:53:48 -0700

    First Commit

---
+++ src/components/Clock.test.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+describe('Clock', function() {
+    it('should have 24 hours', function() {
+        const hours = 24;
+        assert.equal(24, hours);
+    });
+});
