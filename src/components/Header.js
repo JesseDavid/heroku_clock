fc883b7cb75a34adae9457a34798e6f57d98adf5
Steven Shaginyan <sshaginyan@Gmail.com>
2018-09-12 16:30:20 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => (
         <div className="container">
             <div className="row m-auto">
                 <i className="fa fa fa-clock-o fa-4x text-white"></i>
-                <div className="h1 ml-3 my-auto text-light" href="/">{props.title} by Stevn</div>
+                <div className="h1 ml-3 my-auto text-light" href="/">{props.title}</div>
             </div>
         </div>
     </nav>
